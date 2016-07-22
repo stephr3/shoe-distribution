4181fdc9a09d05e9d72e975dda26c1eb291bdca7
Stephanie Gurung <stephr3@gmail.com>
2016-07-22 11:03:58 -0700

    pass specs Brand#validate_presence and Brand#titlecase_name

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,10 @@
 class Brand < ActiveRecord::Base
-  #
+  validates(:name, :presence => true)
+  before_save(:titlecase_name)
+
+private
+
+  define_method(:titlecase_name) do
+    self.name=(name().titlecase())
+  end
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,5 +1,12 @@
 require('spec_helper')
 
 describe(Brand) do
-  #
+  it('validates presence of name') do
+    brand = Brand.new({:name => ''})
+    expect(brand.save()).to(eq(false))
+  end
+  it('converts the name to title case') do
+    brand = Brand.create({:name => 'new balance'})
+    expect(brand.name()).to(eq('New Balance'))
+  end
 end
